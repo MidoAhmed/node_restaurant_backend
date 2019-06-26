b7fd75972aef90d85df702c656c3f1b64ebf8c75
simo <med.elkhalki0@gmail.com>
2019-06-26 10:16:33 +0100

    set mondo db host to 0.0.0.0

--- config/config.js
+++ config/config.js
@@ -8,8 +8,8 @@ CONFIG.port         = process.env.PORT  || '3000';
 
 CONFIG.db_dialect   = process.env.DB_DIALECT    || 'mongo';
 CONFIG.db_protocol  = process.env.db_PROTOCOL   || 'mongodb';
-CONFIG.db_host      = process.env.DB_HOST       || '0.0.0.0';
-CONFIG.db_port      = process.env.DB_PORT       || '27017';
+CONFIG.db_host      = process.env.DB_HOST       || 'database';
+CONFIG.db_port      = process.env.DB_PORT       || '27018';
 CONFIG.db_name      = process.env.DB_NAME       || 'conFusion';
 CONFIG.db_user      = process.env.DB_USER       || 'root';
 CONFIG.db_password  = process.env.DB_PASSWORD   || 'db-password';
