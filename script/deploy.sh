0f69ea7cef6e704527ab438ae0a8d13493cbd4a9
simo <med.elkhalki0@gmail.com>
2019-06-26 16:55:42 +0100

    add deploy step

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-ssh ezderman@159.65.240.12 <<EOF
-  cd ~/node-app
-  git pull
-  npm install --production
-  pm2 restart all
+<<EOF
+  echo 'Hello -------------------> world :)'
   exit
 EOF
