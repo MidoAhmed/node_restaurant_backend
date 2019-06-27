b8b9bb07cd3759ec9bc2e5811c0390ef30e51f01
simo <med.elkhalki0@gmail.com>
2019-06-27 09:52:50 +0100

    add deploy step

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-ssh localhost <<EOF
-  ls
-  echo ${USER}
-  echo 'Hello -------------------> world :)'
-  docker ps
-  docker exec -it ceb9894bd77c bash
-  ls /home/node/node_restaurant_backend
-  exit
-EOF
+echo '-------------------'
+pwd
+echo '-------------------'
+ls
+echo '-------------------'
+echo ${USER}
+echo '-------------------'
+whoami
+echo '-------------------'
